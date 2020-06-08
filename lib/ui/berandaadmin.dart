96265716f038df191c2948b2a40aa274fb080ea3
Suryawan266 <55500482+Suryawan266@users.noreply.github.com>
2020-06-08 14:04:13 +0800

    progress uas 60%

--- lib/ui/berandaadmin.dart
+++ lib/ui/berandaadmin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './akun.dart';
 
 class Berandaadmin extends StatefulWidget{
   @override
@@ -6,14 +8,241 @@ class Berandaadmin extends StatefulWidget{
 }
 
 class BerandaadminState extends State<Berandaadmin> {
+  signOut() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      preferences.setInt("value", null);
+    });
+    Navigator.pushNamed(context, '/login');
+  }
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Beranda Admin"),
+      appBar: new AppBar(
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text('Beranda'),
+        ]),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Click search');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications_active),
+            onPressed: () {
+              print('Click start');
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text("Ini Beranda Admin"),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Ari Suryawan"),
+              accountEmail: new Text("suryaone266@gmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () {},
+                child: new CircleAvatar(
+                  backgroundImage: AssetImage(
+                      'assets/appimages/profil.jpg'),
+                ),
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/appimages/bg_profile.jpg'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            new ListTile(
+              title: new Text('Notifications'),
+              trailing: new Icon(Icons.notifications_none),
+            ),
+            new ListTile(
+              title: new Text('Wishlist'),
+              trailing: new Icon(Icons.bookmark_border),
+            ),
+            new ListTile(
+              title: new Text('Akun'),
+              trailing: new Icon(Icons.verified_user),
+            ),
+            Divider(height: 2,),
+            new ListTile(
+              title: new Text('setting'),
+              trailing: new Icon(Icons.settings),
+            ),
+             new ListTile(
+              title: new Text('Logout'),
+              trailing: new Icon(Icons.settings),
+              onTap: () {
+                signOut();
+              },
+            ),
+          ],
+        ),
+      ),
+//seluruh body dibungkus column
+      body: new ListView(
+        children: <Widget>[
+          Image.asset("assets/appimages/Russian_Blue.jpg"),
+//setiap bagian pada body dipisahkan container
+          Container(
+            color: Colors.blueAccent[100],
+            padding: const EdgeInsets.all(10),
+            //untuk membuat tampilan secara horisontal maka digunakan row
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Rp.1.000.000',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'harga diskon',
+                        style: TextStyle(
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.local_offer,
+                  color: Colors.red[500],
+                ),
+                Text('hemat up to 50%'),
+              ],
+            ),
+          ),
+//setiap bagian pada body dipisahkan container
+          Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Theme.of(context).dividerColor)),
+            ),
+            //untuk membuat tampilan secara horisontal maka digunakan row
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //untuk membuat tampilan secara vertikal maka digunakan column
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.grid_on, color: Colors.blue),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Kategori Kucing",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.shop ,color: Colors.blue),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Makanan Kucing",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.add_call, color: Colors.blue),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Hubungi Kami",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.more, color: Colors.blue),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Lainnya",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: new BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.orange, Colors.yellow],
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Geser ke kiri untuk melihat jenis kucing',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'dapatkan 1 kandang gratis',
+                        style: TextStyle(
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(' Cek Sekarang !'),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
